06d6ad29e33a876173f9488f979e8511371d82da
Nick <nicknewburg@gmail.com>
2019-11-20 15:41:43 -0800

    removed git ignore for deployment

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ var path = require('path');
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, 'views')));
 
 
 const employeeRoute = require("./routes/employeeRoute");
