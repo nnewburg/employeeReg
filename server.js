6a6a17339d1309a69a999a4a184f19dbf293ecc4
Nick Newburg <46974909+nnewburg@users.noreply.github.com>
2019-11-20 18:40:22 -0800

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ var path = require('path');
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static(path.join(__dirname, 'views')));
+app.use(express.static(path.join(__dirname, '/public')));
 
 
 const employeeRoute = require("./routes/employeeRoute");
